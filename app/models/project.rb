bae7a6db64c93ed237c345e6ec6d2a51bb181541
Junichi Ito <jit@sonicgarden.jp>
2024-01-01 08:05:17 +0900

    Define Project#late?

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,8 @@ class Project < ApplicationRecord
   belongs_to :owner, class_name: 'User', foreign_key: :user_id
 
   attribute :due_on, :date, default: -> { Date.current }
+
+  def late?
+    due_on.in_time_zone < Date.current.in_time_zone
+  end
 end
