c471752d845b70c97265fd7014b134ed7dbb96ff
Junichi Ito <jit@sonicgarden.jp>
2023-12-31 22:22:22 +0900

    Add name fields to users rails g migration AddNameFieldsToUsers first_name last_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  def name
+    [first_name, last_name].join(" ")
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_31_131730) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_31_132034) do
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -32,6 +32,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_31_131730) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
