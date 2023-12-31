1364f2c006612e6240571f6ac77fa4326fa43dbc
Junichi Ito <jit@sonicgarden.jp>
2023-12-31 22:18:48 +0900

    Add user model as trackable rails generate devise User

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_31_130416) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_31_131730) do
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.date "due_on"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
