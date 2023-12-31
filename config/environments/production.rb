c949e143bdb2244205e61f23d5439dcec403c7c2
Junichi Ito <jit@sonicgarden.jp>
2023-12-31 21:58:18 +0900

    Rename app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "everydayrails_rspec_jp_2024_production"
+  # config.active_job.queue_name_prefix = "projects_production"
 
   config.action_mailer.perform_caching = false
 
