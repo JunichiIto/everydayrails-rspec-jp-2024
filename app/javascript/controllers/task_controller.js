126c41e43c8d920acd2204b93784d073b6922783
Junichi Ito <jit@sonicgarden.jp>
2024-01-25 19:12:37 +0900

    Hotwire test

--- app/javascript/controllers/task_controller.js
+++ app/javascript/controllers/task_controller.js
@@ -1,9 +1,10 @@
-import Rails from "@rails/ujs"
+// import Rails from "@rails/ujs"
 import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="task"
 export default class extends Controller {
   submit() {
-    Rails.fire(this.element, 'submit');
+    this.element.requestSubmit();
+    // Rails.fire(this.element, 'submit');
   }
 }
